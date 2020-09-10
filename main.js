97cce1adee60de7a76cf1656b4cbeb7c6a4d09e6
HanYoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-09-10 17:07:35 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelector("body").innerHTML = `<div class="tag"></div>`;
+
+const tag = document.querySelector(".tag");
+function updateTag() {
+  tag.innerHTML = `
+  window.screen: ${window.screen.width}, ${window.screen.height} <br/>
+  window.outer: ${window.outerWidth}, ${window.outerHeight} <br/>
+  window.inner: ${window.innerWidth}, ${window.innerHeight} <br/>
+  documentElement.clientWidth: ${document.documentElement.clientWidth}, ${document.documentElement.clientHeight}`;
+}
+
+window.addEventListener("resize", () => updateTag());
+updateTag();
